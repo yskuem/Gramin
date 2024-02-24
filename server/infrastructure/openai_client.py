f8a2c34dfd40773b8a135f8ce563f8f77d2cb559
yskuem <xrayu0022@gmail.com>
2024-02-24 16:30:18 +0900

    update: changed to singleton class

--- server/infrastructure/openai_client.py
+++ server/infrastructure/openai_client.py
@@ -3,12 +3,22 @@ from openai import AsyncOpenAI
 from server.config.openai_config import CREATE_QUIZ_FUNCTION
 
 class OpenAIClient:
-    def __init__(self, api_key):
-        self.client = AsyncOpenAI(
-            api_key = os.environ.get("OPENAI_API_KEY"),
-        )
+    _instance = None
+    _is_initialized = False
 
-    async def generate_quiz(self,sentence : str, model : str = "gpt-4-0125-preview"):
+    def __new__(cls, api_key = None):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, api_key):
+        if not self._is_initialized:
+            self.client = AsyncOpenAI(
+                api_key=os.environ.get("OPENAI_API_KEY", api_key),
+            )
+            self._is_initialized = True
+
+    async def generate_quiz(self, sentence: str, model: str = "gpt-4-0125-preview"):
         return await self.client.chat.completions.create(
             messages=[
                 {
@@ -16,7 +26,7 @@ class OpenAIClient:
                     "content": sentence,
                 }
             ],
-            model = model,
+            model=model,
             functions=[
                 CREATE_QUIZ_FUNCTION
             ],
