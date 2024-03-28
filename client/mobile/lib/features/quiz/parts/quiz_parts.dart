bdd0ad3a3fd941ed6e6750104917ede33f0b7f08
yskuem <xrayu0022@gmail.com>
2024-03-28 11:49:09 +0900

    enable to display ad for ios

--- client/mobile/lib/features/quiz/parts/quiz_parts.dart
+++ client/mobile/lib/features/quiz/parts/quiz_parts.dart
@@ -2,7 +2,9 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_app_template/core/widgets/card/transparent_card.dart';
+import 'package:flutter_app_template/features/advertisement/parts/banner_ad.dart';
 import 'package:flutter_app_template/features/quiz/constants/constants.dart';
 import 'package:flutter_app_template/features/quiz/parts/button_part.dart';
 import 'package:flutter_app_template/features/quiz/use_cases/answered_quiz_controller.dart';
@@ -41,110 +43,118 @@ class QuizParts extends HookConsumerWidget {
     if(quizListData.isEmpty || quizListData.length <= currentQuizIndex.value) {
       return const Center(child: CupertinoActivityIndicator());
     }
-    return Padding(
-      padding: const EdgeInsets.all(15),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Visibility(
-              visible: isCorrect.value != null,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    onPressed: () async {
-                      currentQuizIndex.value++;
-                      isCorrect.value = null;
-                    },
-                    child: const Text(
-                        '次へ',
-                        style: TextStyle(
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Visibility(
+                  visible: isCorrect.value != null,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () async {
+                          currentQuizIndex.value++;
+                          isCorrect.value = null;
+                        },
+                        child: const Text(
+                          '次へ',
+                          style: TextStyle(
                             fontSize: 15,
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
+                          ),
                         ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20,),
+                if(isCorrect.value == null)
+                  const SizedBox(height: 50,),
+                TransparentCard(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 25),
+                    child: Column(
+                      children: [
+                        if(isCorrect.value != null)
+                          _displayResult(isCorrect: isCorrect.value!),
+                        Text(quizListData[currentQuizIndex.value].question,style: quizContentTextStyle,),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20,),
-            if(isCorrect.value == null)
-              const SizedBox(height: 50,),
-            TransparentCard(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 25),
-                child: Column(
-                  children: [
-                    if(isCorrect.value != null)
-                      _displayResult(isCorrect: isCorrect.value!),
-                    Text(quizListData[currentQuizIndex.value].question,style: quizContentTextStyle,),
-                  ],
                 ),
-              ),
-            ),
-            const SizedBox(height: 30,),
-            Visibility(
-              visible: isCorrect.value == null,
-              child: ButtonPart(
-                quizIndex: currentQuizIndex,
-                isCorrect: isCorrect,
-                selectButtonIndex: selectButtonIndex,
-                quizListData: quizListData,
-                currentQuizIndex: currentQuizIndex,
-                updateUserQuizStatus: () async {
-                  // ユーザーのステータスを更新
-                  final userNewState = await ref.read(appUserControllerProvider.notifier).userStateUpdate(
-                    quiz: quizListData[currentQuizIndex.value],
-                    isCorrect: isCorrect.value,
-                  );
-                  await Future.wait([
-
-                    // クイズを取得
-                    ref.read(quizControllerProvider.notifier).fetchMoreQuiz(),
-
-                    // クイズのステータスを更新
-                    ref.read(quizControllerProvider.notifier).answeredQuizUpdate(
-                      quiz: quizListData[currentQuizIndex.value],
-                      selectButtonIndex: selectButtonIndex.value,
-                    ),
-
-                    // 回答済みクイズ記録を保存
-                    ref.read(answeredQuizControllerProvider.notifier).save(
-                      quizId: quizListData[currentQuizIndex.value].id,
-                      userSelectIndex: selectButtonIndex.value,
-                      isCorrect: isCorrect.value,
-                    ),
-                  ]);
-
-                  // ランキングのステータスを更新
-                  await ref.read(rankingControllerProvider.notifier).updateRankingUser(
-                    user: userNewState,
-                  );
-                },
-              ),
-            ),
-            Visibility(
-              visible: isCorrect.value != null,
-              child: TransparentCard(
-                opacity: 0.6,
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: ExplanationPart(
+                const SizedBox(height: 30,),
+                Visibility(
+                  visible: isCorrect.value == null,
+                  child: ButtonPart(
                     quizIndex: currentQuizIndex,
+                    isCorrect: isCorrect,
                     selectButtonIndex: selectButtonIndex,
+                    quizListData: quizListData,
+                    currentQuizIndex: currentQuizIndex,
+                    updateUserQuizStatus: () async {
+                      // ユーザーのステータスを更新
+                      final userNewState = await ref.read(appUserControllerProvider.notifier).userStateUpdate(
+                        quiz: quizListData[currentQuizIndex.value],
+                        isCorrect: isCorrect.value,
+                      );
+                      await Future.wait([
+
+                        // クイズを取得
+                        ref.read(quizControllerProvider.notifier).fetchMoreQuiz(),
+
+                        // クイズのステータスを更新
+                        ref.read(quizControllerProvider.notifier).answeredQuizUpdate(
+                          quiz: quizListData[currentQuizIndex.value],
+                          selectButtonIndex: selectButtonIndex.value,
+                        ),
+
+                        // 回答済みクイズ記録を保存
+                        ref.read(answeredQuizControllerProvider.notifier).save(
+                          quizId: quizListData[currentQuizIndex.value].id,
+                          userSelectIndex: selectButtonIndex.value,
+                          isCorrect: isCorrect.value,
+                        ),
+                      ]);
+
+                      // ランキングのステータスを更新
+                      await ref.read(rankingControllerProvider.notifier).updateRankingUser(
+                        user: userNewState,
+                      );
+                    },
                   ),
                 ),
-              ),
+                Visibility(
+                  visible: isCorrect.value != null,
+                  child: TransparentCard(
+                    opacity: 0.6,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: ExplanationPart(
+                        quizIndex: currentQuizIndex,
+                        selectButtonIndex: selectButtonIndex,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
+        const Align(
+            alignment: Alignment.bottomCenter,
+            child: BannerAdPart(),
+        ),
+      ],
     );
   }
 
