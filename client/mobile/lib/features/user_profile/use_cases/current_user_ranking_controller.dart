55bbbc5d2f9c06851e3b030d1df9909e09b1a844
yskuem <xrayu0022@gmail.com>
2024-03-29 01:13:36 +0900

    add: current user ranking provider

---
+++ client/mobile/lib/features/user_profile/use_cases/current_user_ranking_controller.dart
@@ -0,0 +1,42 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../core/exceptions/app_exception.dart';
+import '../../../core/repositories/firebase_auth/firebase_auth_repository.dart';
+import '../../app_user/entities/app_user.dart';
+import '../../app_user/use_case/app_user_controller.dart';
+part 'current_user_ranking_controller.g.dart';
+
+@Riverpod(keepAlive: true)
+Future<int> fetchAllUserCount(FetchAllUserCountRef ref) async {
+  final snap = await FirebaseFirestore.instance
+      .collection(AppUser.collectionPath)
+      .count()
+      .get();
+  return snap.count ?? 0;
+}
+
+
+@riverpod
+Future<int> fetchCurrentUserRanking(
+    FetchCurrentUserRankingRef ref,
+) async {
+  final currentUser = await ref.read(appUserControllerProvider.future);
+  final userId = ref.read(firebaseAuthRepositoryProvider).loggedInUserId;
+  if (userId == null || currentUser == null) {
+    throw AppException(title: 'ログインしてください');
+  }
+  final snap = await FirebaseFirestore.instance
+      .collection(AppUser.collectionPath)
+      .where('exPoint', isGreaterThan: currentUser.exPoint)
+      .count()
+      .get();
+  final num = snap.count;
+  if(num == null) {
+    return 0;
+  }
+  final rank = num + 1;
+  return rank;
+}
---
+++ client/mobile/lib/features/user_profile/use_cases/current_user_ranking_controller.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'current_user_ranking_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchAllUserCountHash() => r'a5e3b5beeea67600e28134d756bca6b2cfb393f1';
+
+/// See also [fetchAllUserCount].
+@ProviderFor(fetchAllUserCount)
+final fetchAllUserCountProvider = FutureProvider<int>.internal(
+  fetchAllUserCount,
+  name: r'fetchAllUserCountProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fetchAllUserCountHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FetchAllUserCountRef = FutureProviderRef<int>;
+String _$fetchCurrentUserRankingHash() =>
+    r'69f00487f5d9f55c11e74e30403eba4bd66d04f0';
+
+/// See also [fetchCurrentUserRanking].
+@ProviderFor(fetchCurrentUserRanking)
+final fetchCurrentUserRankingProvider = AutoDisposeFutureProvider<int>.internal(
+  fetchCurrentUserRanking,
+  name: r'fetchCurrentUserRankingProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$fetchCurrentUserRankingHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FetchCurrentUserRankingRef = AutoDisposeFutureProviderRef<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
