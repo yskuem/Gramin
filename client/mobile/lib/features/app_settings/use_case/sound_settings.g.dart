21527db5c03a26b313ef7a30a730119cd171c169
yskuem <xrayu0022@gmail.com>
2024-06-09 18:59:40 +0900

    fix: lint warning

---
+++ client/mobile/lib/features/app_settings/use_case/sound_settings.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sound_settings.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$soundSettingHash() => r'03b4a4589c6ea752aa65b883621735793606100b';
+
+/// See also [SoundSetting].
+@ProviderFor(SoundSetting)
+final soundSettingProvider =
+    AutoDisposeAsyncNotifierProvider<SoundSetting, bool>.internal(
+  SoundSetting.new,
+  name: r'soundSettingProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$soundSettingHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SoundSetting = AutoDisposeAsyncNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
